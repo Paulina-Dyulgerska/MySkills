75981730aa0d21e42de36ff0f3826cccced7d852
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-04-07 20:34:33 +0300

    Experiance card style initial

--- src/components/Experiance/ExperianceCard/ExperianceCard.js
+++ src/components/Experiance/ExperianceCard/ExperianceCard.js
@@ -1,13 +1,17 @@
 import './ExperianceCard.css';
 
+import LogoNavigationItem from '../../Header/NavigationItem/LogoNavigationItem';
+
 const ExperianceCard = () => {
     return (
-        <div className="media exprience-item">
-            <div className="media-left"><a href=".#">
-                <img src="/static/media/design-logo4.cc583d9d.png" alt="" /></a>
+        <div className="exprience-item">
+            <div className="media-left">
+                <a href="https://www.liebherr.com">
+                    <img src="https://www.liebherr.com/media/global/img/svg/logo_ci_liebherr.svg" alt="Liebherr" />
+                </a>
             </div>
             <div className="media-body">
-                <h4>Graphic Designer (intern)</h4>
+                <h4>Liebherr</h4>
                 <span>March, 2015 - June, 2017 (1 year, 3 Month)</span>
                 <p>Proven ability to lead and manage a wide variety of design and development projects in team and independent situations.</p>
             </div>
