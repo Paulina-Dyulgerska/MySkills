3c899b96fdf301d6938e392e14e6b53ef49cc590
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-04-24 21:09:17 +0300

    Contact page fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,82 +8,83 @@ const Contact = () => {
     return (
         <section className="contact-area-wrapper">
             <section className="contact-area-container">
-                <article className="contact-area-content">
-                    <TextBlockContent
-                        title="Talk Or Meet Me"
-                        primary={["Get in touch"]}
-                        secondary={[]}
-                    >
-                    </TextBlockContent>
+                <TextBlockContent
+                    title="Talk Or Meet Me"
+                    primary={["Get in touch"]}
+                    secondary={[]}
+                >
+                </TextBlockContent>
 
-                    <MediaItem
-                        key='1'
-                        title='Call Me Now'
-                        link=''
-                        linkText='02934567845'
-                        details=''
-                    />
-                    <MediaItem
-                        key='2'
-                        title='Contact Me'
-                        link=''
-                        linkText='john@gmail.com'
-                        details=''
-                    />
-                    <MediaItem
-                        key='3'
-                        title='Get Me Here'
-                        link=''
-                        linkText='Z105 - London, UK'
-                        details=''
-                    />
-                    <MediaItem
-                        key='4'
-                        title='Listen To Me'
-                        link=''
-                        linkText='john.com'
-                        details=''
-                    />
-                </article>
-                <article className="contact-area-form fadeInRight">
-                    <TextBlockContent
-                        title="Let Me Know"
-                        primary={["Contact me"]}
-                        secondary={[]}
-                    >
-                    </TextBlockContent>
-
-                    <form className="contact-area-form">
-                        <article className="input-fields">
-                            <span className="input">
-                                <input type="text" id="name" name="name" className="form-control" placeholder="Your Name*" />
-                                <span className="actions"></span>
-                            </span>
-                            <span className="input">
-                                <input type="email" id="email" name="email" className="form-control" placeholder="Your Email*" />
-                                <span className="actions"></span>
-                            </span>
-                            <span className="input">
-                                <input type="text" id="subject" name="subject" className="form-control" placeholder="Subject*" />
-                                <span className="actions"></span>
-                            </span>
-                            <span className="input">
-                                <input type="text" id="phone" name="phone" className="form-control" placeholder="Phone*" />
-                                <span className="actions"></span>
-                            </span>
-                        </article>
-                        <span className="input">
-                            <textarea name="message" id="message" name="testarea" className="form-control" rows="6" placeholder="Your Message ..."></textarea>
-                        </span>
-                        <ButtonSubmit
-                            path={`#`}
-                        >
-                            Send Message
-                        </ButtonSubmit>
-                    </form>
-                </article>
+                <MediaItem
+                    key='1'
+                    icon='fas fa-phone-volume'
+                    title='Call Me Now'
+                    link=''
+                    linkText='+359887684868'
+                />
+                <MediaItem
+                    key='2'
+                    icon='fas fa-envelope-open-text'
+                    title='Contact Me'
+                    link=''
+                    linkText='paylina_st@yahoo.com'
+                    details=''
+                />
+                <MediaItem
+                    key='3'
+                    icon='fas fa-street-view'
+                    title='Get Me Here'
+                    link=''
+                    linkText='4003 - Plovidv, Bulgaria'
+                    details=''
+                />
+                <MediaItem
+                    key='4'
+                    icon='fas fa-headphones'
+                    title='Listen To Me'
+                    link=''
+                    linkText='paulinaweb.com; dotnetweb.net'
+                    details=''
+                />
             </section>
-        </section >
+            <section className="contact-area-form fadeInRight">
+                <TextBlockContent
+                    title="Let Me Know"
+                    primary={["Contact me"]}
+                    secondary={[]}
+                >
+                </TextBlockContent>
+
+                <form className="contact-area-form">
+                    <article className="input-fields">
+                        <span className="input">
+                            <input type="text" id="name" name="name" className="form-control" placeholder="Your Name*" />
+                            <span className="actions"></span>
+                        </span>
+                        <span className="input">
+                            <input type="email" id="email" name="email" className="form-control" placeholder="Your Email*" />
+                            <span className="actions"></span>
+                        </span>
+                        <span className="input">
+                            <input type="text" id="subject" name="subject" className="form-control" placeholder="Subject*" />
+                            <span className="actions"></span>
+                        </span>
+                        <span className="input">
+                            <input type="text" id="phone" name="phone" className="form-control" placeholder="Phone*" />
+                            <span className="actions"></span>
+                        </span>
+                    </article>
+                    <span className="input">
+                        <textarea name="message" id="message" name="testarea" className="form-control" rows="6" placeholder="Your Message ..."></textarea>
+                    </span>
+                    <ButtonSubmit
+                        path={`#`}
+                    >
+                        Send Message
+                        </ButtonSubmit>
+                </form>
+            </section>
+        </section>
     )
 }
 
--- src/components/Contact/MediaItem/MediaItem.js
+++ src/components/Contact/MediaItem/MediaItem.js
@@ -1,19 +1,18 @@
 import './MediaItem.css';
 
 const MediaItem = ({
+    icon,
     title,
     link,
     linkText,
-    details,
 }
 ) => {
     return (
         <article className="media-item">
-            <i className="flaticon-phone"></i>
+            <i className={icon}></i>
             <article className="media-item-body">
                 <h6>{title}</h6>
                 <a href={link}>{linkText}</a>
-                <p>{details}</p>
             </article>
         </article>
     )
