5647c7d2dfa903dab352a571bf82b5064816a6cd
Paulina-Dyulgerska <paylina_st@yahoo.com>
2021-05-29 15:58:21 +0300

    Menu toggle styles fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 // import { Link } from 'react-router-dom';
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from "../../img/logo.png";
 import "./Header.css";
 
@@ -10,29 +10,19 @@ import UserMenu from './UserMenu/UserMenu';
 const Header = (props) => {
     const menuToggler = useRef(null);
 
-    const [gh, setGH] = useState(false);
+    const [isMenuTogglerChecked, setIsMenuTogglerChecked] = useState(false);
 
-    const onClickNavMenuHandler = () => {
-        setGH(!gh);
-        console.log(gh, 'onclick');
-        console.log(menuToggler);
-    }
-
-    useEffect(() => {
-    }, [gh]);
-
-    const onChange = () => {
-        setGH(!gh);
-        console.log(gh, 'onchange');
+    const changeMenuTogglerCheckedStatus = () => {
+        setIsMenuTogglerChecked(!isMenuTogglerChecked);
     }
 
     return (
         <header className="header-wrapper" >
             <section className="content-wrapper header-content">
 
-                <input onChange={onChange}
-                    ref={menuToggler} checked={gh} 
-                    className="nav-toggler" type="checkbox" name="toggle-nav" id="toggle-nav"  />
+                <input onChange={changeMenuTogglerCheckedStatus}
+                    ref={menuToggler} checked={isMenuTogglerChecked}
+                    className="nav-toggler" type="checkbox" name="toggle-nav" id="toggle-nav" />
 
                 {/* <div className="sticky-outer-wrapper">
                 <div className="sticky-inner-wrapper">
@@ -52,8 +42,8 @@ const Header = (props) => {
                     <i className="fas fa-bars"></i> Menu
                 </label>
 
-                <nav className="nav" >
-                    <ul className="nav-list nav-left" onClick={onClickNavMenuHandler}>
+                <nav className="nav" onClick={changeMenuTogglerCheckedStatus}>
+                    <ul className="nav-list nav-left">
                         <NavigationItem path="/home">Home</NavigationItem>
                         <NavigationItem path="/about">About</NavigationItem>
                         <NavigationItem path="/education">Education</NavigationItem>
@@ -70,3 +60,70 @@ const Header = (props) => {
 }
 
 export default Header;
+
+
+
+
+// const Header = (props) => {
+//     const menuToggler = useRef(null);
+
+//     const [gh, setGH] = useState(false);
+
+//     const onClickNavMenuHandler = () => {
+//         setGH(!gh);
+//         console.log(gh, 'onclick');
+//         console.log(menuToggler);
+//     }
+
+//     useEffect(() => {
+//     }, [gh]);
+
+//     const onChange = () => {
+//         setGH(!gh);
+//         console.log(gh, 'onchange');
+//     }
+
+//     return (
+//         <header className="header-wrapper" >
+//             <section className="content-wrapper header-content">
+
+//                 <input onChange={onChange}
+//                     ref={menuToggler} checked={gh} 
+//                     className="nav-toggler" type="checkbox" name="toggle-nav" id="toggle-nav"  />
+
+//                 {/* <div className="sticky-outer-wrapper">
+//                 <div className="sticky-inner-wrapper">
+
+//                 </div>
+//                  </div> */}
+//                 <article className="site-title">
+//                     <LogoNavigationItem
+//                         path="/"
+//                         src={logo} alt="Logo"
+//                         className="logo-img"
+//                         logoText="Paulina D.">
+//                     </LogoNavigationItem>
+//                 </article>
+
+//                 <label className="toggle-nav" htmlFor="toggle-nav">
+//                     <i className="fas fa-bars"></i> Menu
+//                 </label>
+
+//                 <nav className="nav" >
+//                     <ul className="nav-list nav-left" onClick={onClickNavMenuHandler}>
+//                         <NavigationItem path="/home">Home</NavigationItem>
+//                         <NavigationItem path="/about">About</NavigationItem>
+//                         <NavigationItem path="/education">Education</NavigationItem>
+//                         <NavigationItem path="/experience">Experience</NavigationItem>
+//                         <NavigationItem path="/portfolio">Portfolio</NavigationItem>
+//                         <NavigationItem path="/blog">Blog</NavigationItem>
+//                         <NavigationItem path="/contact">Contact</NavigationItem>
+//                     </ul>
+//                     <UserMenu isUserLoggedIn={props.isUserLoggedIn}></UserMenu>
+//                 </nav>
+//             </section>
+//         </header >
+//     )
+// }
+
+// export default Header;
